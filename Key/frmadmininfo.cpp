9f65760bd3007b02f667cb60188d9b952544f8d4
Rich Claussen <rich@theclaussens.com>
2017-05-14 23:33:35 -0700

    Update database to hold assist info and "show fingerprint"

--- Key/frmadmininfo.cpp
+++ Key/frmadmininfo.cpp
@@ -1034,6 +1034,9 @@ void CFrmAdminInfo::on_btnSaveSettings_clicked()
     _tmpAdminRec.setEmailReportActive(bNever);
     _tmpAdminRec.setPassword(ui->lblPassword->text().toStdString());
     _tmpAdminRec.setAccessCode(ui->lblAccessCode->text().toStdString());
+    _tmpAdminRec.setAssistPassword(ui->lblAssistPassword->text().toStdString());
+    _tmpAdminRec.setAssistCode(ui->lblAssistCode->text().toStdString());
+    _tmpAdminRec.setShowFingerprint(ui->chkShowFingerprint->isChecked());
     _tmpAdminRec.setUsePredictiveAccessCode(ui->chkUsePredictive->isChecked());
     _tmpAdminRec.setPredictiveKey(ui->lblKey->text().toStdString());
     _tmpAdminRec.setPredictiveResolution(ui->spinCodeGenResolution->value());
@@ -1081,6 +1084,9 @@ void CFrmAdminInfo::on_btnDone_clicked()
     _tmpAdminRec.setEmailReportActive(bNever);
     _tmpAdminRec.setPassword(ui->lblPassword->text().toStdString());
     _tmpAdminRec.setAccessCode(ui->lblAccessCode->text().toStdString());
+    _tmpAdminRec.setAssistPassword(ui->lblAssistPassword->text().toStdString());
+    _tmpAdminRec.setAssistCode(ui->lblAssistCode->text().toStdString());
+    _tmpAdminRec.setShowFingerprint(ui->chkShowFingerprint->isChecked());
     _tmpAdminRec.setUsePredictiveAccessCode(ui->chkUsePredictive->isChecked());
     _tmpAdminRec.setPredictiveKey(ui->lblKey->text().toStdString());
     _tmpAdminRec.setPredictiveResolution(ui->spinCodeGenResolution->value());
--- Key/tbladmin.h
+++ Key/tbladmin.h
@@ -21,11 +21,14 @@ private:
     std::string password;    // text,
     std::string access_code_old; // text
     std::string access_code; // text
+    std::string assist_code; //text
+    std::string assist_password; //text
     bool        use_predictive_access_code;
     std::string pred_key;
     int         pred_resolution;
     uint32_t    max_locks;
 
+    bool        show_fingerprint; // bool: true = display button
     std::string smtp_server;
     int smtp_port;
     int smtp_type;
@@ -54,17 +57,20 @@ public:
         password = "";
         access_code_old = "";
         access_code = "";
+        assist_code = "";
+        assist_password = "";
         use_predictive_access_code = false;
         pred_key = "KEYCODEBOXTEST";
         pred_resolution = 10;
         max_locks = 16;
 
+        show_fingerprint = 0;
         smtp_server = "";
         smtp_port = 0;
         smtp_type = 0; // 0=TCP, 1=SSL, 2=TSL
         smtp_username = "";
         smtp_password = "";
-	vnc_server = "";
+        vnc_server = "";
         vnc_port = 0;
         vnc_type = 0; // 0=TCP, 1=SSL, 2=TSL
         vnc_username = "";
@@ -87,19 +93,22 @@ public:
         password = newRec.password;
         access_code_old = newRec.access_code_old;
         access_code = newRec.access_code;
+        assist_code = newRec.assist_code;
+        assist_password = newRec.assist_password;
         use_predictive_access_code = newRec.use_predictive_access_code;
         pred_key = newRec.pred_key;
         pred_resolution = newRec.pred_resolution;
         max_locks = newRec.max_locks;
 
+        show_fingerprint = newRec.show_fingerprint;
         smtp_server = newRec.smtp_server;
         smtp_port = newRec.smtp_port;
         smtp_type = newRec.smtp_type;
         smtp_username = newRec.smtp_username;
         smtp_password = newRec.smtp_password;
 
-	vnc_port = newRec.vnc_port;
-	vnc_password = newRec.vnc_password;
+        vnc_port = newRec.vnc_port;
+	    vnc_password = newRec.vnc_password;
 	
         report_via_email = newRec.report_via_email;
         report_save_to_file = newRec.report_save_to_file;
@@ -108,24 +117,40 @@ public:
 
     int getID() { return ids; }    //  integer primary key unique,
     void setID(int id) { ids = id; }
+
     std::string getAdminName() { return admin_name; }// text,
     void setAdminName(std::string adminName) { admin_name = adminName; }
+
     std::string getAdminEmail() { return admin_email; } // text,
     void setAdminEmail(std::string adminEmail) { admin_email = adminEmail; }
+
     std::string getAdminPhone() { return admin_phone; } // text,
     void setAdminPhone(std::string adminPhone) { admin_phone = adminPhone; }
+
     bool getEmailReportActive() { return email_report_active; }
     void setEmailReportActive(bool bActive) { email_report_active = bActive; }
+
     QDateTime getDefaultReportFreq() { return default_report_freq; } // DATETIME
     void  setDefaultReportFreq(QDateTime freq) { default_report_freq = freq; }
+
     QDateTime getDefaultReportStart() { return default_report_start; }    // DATETIME
     void setDefaultReportStart(QDateTime start) { default_report_start = start; }    // DATETIME
+
     std::string getPassword() { return password; }   // text,
     void setPassword(std::string pw) { password = pw; }
+
     std::string getAccessCodeOld() { return access_code_old; } // text
     void setAccessCodeOld(std::string accessCode) { access_code_old = accessCode; }
+
     std::string getAccessCode() { return access_code; } // text
     void setAccessCode(std::string accessCode) { access_code = accessCode; }
+
+    std::string getAssistCode() { return assist_code; } //text
+    void setAssistCode(std::string assistCode) { assist_code = assistCode; }
+
+    std::string getAssistPassword() { return assist_password; } //text
+    void setAssistPassword(std::string assistPassword) { assist_password = assistPassword; }
+
     bool getUsePredictiveAccessCode() { return use_predictive_access_code; }
     void setUsePredictiveAccessCode(bool bSet) { use_predictive_access_code = bSet; }
 
@@ -140,12 +165,19 @@ public:
 
     std::string getSMTPServer() { return smtp_server; }
     void setSMTPServer(std::string smtpserver) { smtp_server = smtpserver; }
+
+    bool getShowFingerprint() { return show_fingerprint; }
+    void setShowFingerprint(bool showFingerprint) { show_fingerprint = showFingerprint; }
+
     int getSMTPPort() { return smtp_port; }
     void setSMTPPort(int port) { smtp_port = port; }
+
     int getSMTPType() { return smtp_type; }
     void setSMTPType(int type) { smtp_type = type; }
+
     std::string getSMTPUsername() { return smtp_username; }
     void setSMTPUsername(std::string smtpusername) { smtp_username = smtpusername; }
+
     std::string getSMTPPassword() { return smtp_password; }
     void setSMTPPassword(std::string password) { smtp_password = password; }
 
@@ -187,7 +219,9 @@ public:
  *      default_report_freq TIME,
  *      default_report_start DATETIME,
  *      password text,
- *      access_code text
+ *      access_code text,
+ *      assist_code text,
+ *      assist_password text
  */
 
 class CTblAdmin
@@ -225,20 +259,25 @@ public:
     bool updateAdmin(std::string name, std::string email, std::string phone,
                      bool activeReport, QDateTime repFreq, QDateTime startReport,
                      std::string passwordEnc, std::string accessCdEnc,
+                     std::string assistPasswordEnc, std::string assistCodeEnc,
+                     bool showFingerprint,
                      bool usePredictive, std::string predKey, int predRes,
                      uint32_t nMaxLocks,
                      std::string smtpserver, int smptport, int smtptype,
                      std::string smtpusername, std::string smtppassword,
-		     int vncport, std::string vncpasword,
+                     int vncport, std::string vncpasword,
                      bool bEmailReport, bool bSaveReport, std::string reportDirectory);
+
     bool updateAdminClear(std::string name, std::string email, std::string phone,
                      bool activeReport, QDateTime repFreq, QDateTime startReport,
                      std::string passwordClear, std::string accessCdClear,
+                     std::string assistPasswordClear, std::string assistCodeClear,
+                     bool showFingerprint,
                      bool usePredictive, std::string predKey, int predRes,
                      uint32_t nMaxLocks,
                      std::string smtpserver, int smptport, int smtptype,
                      std::string smtpusername, std::string smtppassword,
-		     int vncport, std::string vncpassword,
+                     int vncport, std::string vncpassword,
                      bool bEmailReport, bool bSaveReport, std::string reportDirectory);
     bool updateAdmin(CAdminRec &rec);
     bool updateAdmin(QJsonObject adminObj);
